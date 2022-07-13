17b8e8c87fdaeeb395dd41fe9a10859b7c7edb62
Coleman B Gentry <104287033+colemanbgentry@users.noreply.github.com>
2022-07-13 10:53:26 -0700

    Able to get everything to work.

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
+//Import list
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
 
+//Going to the intended screen, use Expo Go, with a QR instruction to see the app
 export default function App() {
+  //Expressions
+  const [count, setCount] = useState(0);
+  const onIncrease = () => setCount(prevCount => prevCount + 1);
+  const onDecrease = () => setCount(prevCount => prevCount - 1);
+  const reset      = () => setCount(prevCount => prevCount * 0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //The SafeAreaView allows the app not to 
+    // extend into the status bar on all phones.
+    <SafeAreaView style={[styles.container]}>
+      <View style={[{flex: 7}, styles.counterBox]}>
+        <Text style={[styles.counter]}>
+          {count}
+        </Text>
+      </View>
+      <View style={[{flex: 1}, styles.buttonShelf]}>
+        <TouchableOpacity 
+          onPress={onDecrease}
+          style={[{flex: 1}, styles.buttonValue]}
+          >
+          <Text style={[styles.buttons]}>
+            -
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={onIncrease}
+          style={[{flex: 1}, styles.buttonValue]}
+          >
+          <Text style={[styles.buttons]}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={[{flex: 1}, styles.buttonReset, styles.buttonShelf]}>
+        <TouchableOpacity
+          onPress={reset}
+          style={[styles.buttonValue]}
+        >
+          <Text style={[styles.buttons]}>
+            Reset
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/*
+          I had to figure out the inline React commenting which was different
+          StatusBar is supposed to be auto, I was having problems with it and 
+          set it to light instead. 
+      */}
+      <StatusBar style="light" /> 
+    </SafeAreaView>
   );
 }
-
+//StyleSheet for the app
 const styles = StyleSheet.create({
+  //Holds the main portion of the app
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+    flexDirection: 'column'
+  },
+  //Holds the button level
+  buttonShelf: {
+    flexDirection: "row",
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  //Gives the buttons their flavor
+  buttonValue: {
+    margin: 2,
+    padding: 20,
+    backgroundColor: "blue",
+    alignSelf: "center",
+  },
+  //Wanting the reset button to fill the bottom part of the screen
+  //Just setteled for what I got instead
+  buttonReset: {
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  //Giving the button their size and font
+  buttons: {
+    fontSize: 25,
+    color: "#fff",
+    textAlign: 'center',
+  },
+  //Making the number box for alignment
+  counterBox: {
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  //Making the number big enough to see
+  counter: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "#fff"
   },
 });
---
+++ README.md
@@ -0,0 +1,32 @@
+# Overview
+
+I wanted to create a simple app to try my hand at React Native with the help of the Expo Cli client page and Expo Go app. 
+
+This app is a counting app, allowing the user to increase or decrease a value on screen. With two buttons close to the button of the screen one can increase and decrease the number.I did add a reset button to allow the user to go back to zero if necessary.
+
+I wanted to make a app that allowed me to change a value for a game. 
+
+Here is a link to the video showing the demonstration of the app, then the code.
+
+[Tap Counter App Demonstration](https://youtu.be/f6Bd2apwkJ8)
+
+# Development Environment
+
+I used React Native, through Yarn and Expo. 
+
+React Native is at a basic level JavaScript a HTML format language. Yarn is a package depenency management that can help hold packages for Expo and React. Expo hosted the server that can also interpret React Native.
+
+# Useful Websites
+
+A few Websites that I used for this app to guide and help me are...
+
+* [React Native](https://reactnative.dev/)
+* [Expo](https://expo.dev/)
+* [Yarn](https://classic.yarnpkg.com/en/)
+
+# Future Work
+
+I struggled a bit to make it work and I had to give up on things that I couldn't figure out yet. So in the future I will like to add these few things.
+* To remove the blue buttons and turn the whole screen into a button. Allowing the minus button and plus button to take each half of the screen.
+* To have different counting methods, on paper I wanted to figure out a way to make a tally point system for simple board games. At my current skill level, I went with simple numbers. 
+* Using dark screens are my favorite, but I know that people do not prefer them, so later will figure out how to get the preset setting for light and dark to affect the app. 
